195078301f0911fe6f3c071544f3a5910127969a
Basemera <phiona.basemera@andela.com>
2018-10-05 21:02:42 +0300

    Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,6 +5,13 @@ import QuizEnd from "./QuizEnd";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
+  showNextQuestion = () => {
+    this.setState(state => {
+      return {
+        quiz_position: state.quiz_position + 1
+      };
+    });
+  };
   constructor(props) {
     super(props);
     this.state = {
@@ -23,6 +30,7 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
